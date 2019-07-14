8af579975cf3ca22e737f7dbc5a234b7ca14ea64
hefy <282971962@qq.com>
2019-07-14 13:05:09 +0800

    char 类型的赋值细节

---
+++ Java_02_操作符/src/com/hefy/Test_1_CharTest.java
@@ -0,0 +1,13 @@
+package com.hefy;
+
+/**
+ * @author hefy:
+ * @date 2019年7月14日 下午1:03:51
+ * @version 1.0 类说明  c 变量为 char 类型，65 对应的 ASCII 码为 A。
+ */
+public class Test_1_CharTest {
+	public static void main(String[] args) {
+		char c = 65;
+		System.out.println("c = " + c); //c = A
+	}
+}
